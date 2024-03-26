f7b53b111f212f1c4d35220d39e0fcce0ab87db1
aleksey-grigorev <aleksey-grigorev7@mail.ru>
2024-03-26 19:30:05 +0300

    Доделал профиль

--- client/src/components/PostItem/PostsItem.tsx
+++ client/src/components/PostItem/PostsItem.tsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import styles from './PostItem.module.css'
+import React, { useState } from 'react';
+import styles from './PostItem.module.css';
+import { GoHeart, GoHeartFill, GoComment } from 'react-icons/go';
+import { TbShare3 } from 'react-icons/tb';
+
 interface PostItemProps {
-    img: string;
-    authorName: string;
-    postBody: string;
+  img: string;
+  authorName: string;
+  postBody: string;
 }
 
-const PostsItem : React.FC<PostItemProps> = ({
-    img,
-    authorName,
-    postBody
-}) => {
-    return (
-        <div className={styles.post}>
-            <div className={styles.postTitle}>
-                <div className={styles.imgContainer}>
-                    <img
-                        src={img}
-                        alt=''
-                    />
-                </div>
-                <div className={styles.authorNameContainer}>
-                    <div className={styles.authorName}>{authorName}</div>
-                </div>
-            </div>
-            <div className={styles.postBody}>
-                <div>
-                    {postBody}
-                </div>
-            </div>
+const PostsItem: React.FC<PostItemProps> = ({ img, authorName, postBody }) => {
+  const [isLiked, switchIsLiked] = useState(false);
+  return (
+    <div className={styles.post}>
+      <div className={styles.postTitle}>
+        <div className={styles.imgContainer}>
+          <img src={img} alt='' />
         </div>
-    );
+        <div className={styles.authorNameContainer}>
+          <div className={styles.authorName}>{authorName}</div>
+        </div>
+      </div>
+      <div className={styles.postBody}>
+        <div>{postBody}</div>
+      </div>
+      <div className={styles.actionsContainer}>
+        {isLiked ? (
+          <button
+            className={styles.likeBtnActive + ' ' + styles.actionBtn}
+            onClick={() => {
+              switchIsLiked(false);
+            }}
+          >
+            <GoHeartFill />
+          </button>
+        ) : (
+          <button
+            className={styles.likeBtnInactive + ' ' + styles.actionBtn}
+            onClick={() => {
+              switchIsLiked(true);
+            }}
+          >
+            <GoHeart />
+          </button>
+        )}
+        <button className={styles.actionBtn}>
+          <GoComment />
+        </button>
+        <button className={styles.actionBtn}>
+          <TbShare3 />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PostsItem;
