9bfb2ac923974af81729016e7dbe2f39d973645a
aleksey-grigorev <aleksey-grigorev7@mail.ru>
2024-04-12 00:19:48 +0300

    Добавил возможность запускать приложение в docker контейнере с помощью docker-compose.yml

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -3,31 +3,30 @@ import HomePage from '../pages/HomePage.tsx';
 import ProfilePage from '../pages/ProfilePage.tsx';
 import DialogsPage from '../pages/DialogsPage.tsx';
 import FriendsPage from '../pages/FriendsPage.tsx';
-import LoginPage from "../pages/LoginPage.tsx";
-import RegistrationPage from "../pages/RegistrationPage.tsx";
+import LoginPage from '../pages/LoginPage.tsx';
+import RegistrationPage from '../pages/RegistrationPage.tsx';
 
 interface IRoute {
-    path: string;
-    element: React.ReactNode;
+  path: string;
+  element: React.ReactNode;
 }
 
 export enum RouteNames {
-    HOME = '/',
-    PROFILE = '/profile',
-    DIALOGS = '/dialogs',
-    FRIENDS = '/friends',
-    LOGIN = '/login',
-    REGISTRATION = '/registration'
+  HOME = '/',
+  PROFILE = '/profile',
+  DIALOGS = '/dialogs',
+  FRIENDS = '/friends',
+  LOGIN = '/login',
+  REGISTRATION = '/registration',
 }
 
 export const PUBLIC_ROUTES: IRoute[] = [
-    {path: RouteNames.LOGIN, element: <LoginPage/>},
-    {path: RouteNames.REGISTRATION, element: <RegistrationPage/>}
-]
+  { path: RouteNames.LOGIN, element: <LoginPage /> },
+  { path: RouteNames.REGISTRATION, element: <RegistrationPage /> },
+];
 export const PRIVATE_ROUTES: IRoute[] = [
-    {path: RouteNames.HOME, element: <HomePage/>},
-    {path: RouteNames.PROFILE, element: <ProfilePage/>},
-    {path: RouteNames.DIALOGS, element: <DialogsPage/>},
-    {path: RouteNames.FRIENDS, element: <FriendsPage/>},
-
+  { path: RouteNames.HOME, element: <HomePage /> },
+  { path: RouteNames.PROFILE, element: <ProfilePage /> },
+  { path: RouteNames.DIALOGS, element: <DialogsPage /> },
+  { path: RouteNames.FRIENDS, element: <FriendsPage /> },
 ];
